d98807692b0a6e5ca152441206cf642faa183e83
sherpa-code <josephcrane@live.com>
2021-03-30 05:49:04 -0230

    Item System UI
    - Added Item UI and prefabs

---
+++ Assets/Scripts/ItemScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public enum ItemType
+    {
+        HEAL,
+        REVIVE,
+        BUFF,
+        ANTIDOTE
+    }
+
+    public string itemName;
+    public int itemEffectiveness;
+    
+}
